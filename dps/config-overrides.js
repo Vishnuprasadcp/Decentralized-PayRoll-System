1675039ea2c48f118ae4682c1f75d0db3ccff4e3
Vishnuprasadcp <vishnuprasadcp07@gmail.com>
2025-02-03 22:25:46 +0530

    new change

--- dps/config-overrides.js
+++ dps/config-overrides.js
@@ -5,7 +5,6 @@ module.exports = function override(config) {
   Object.assign(fallback, {
     crypto: require.resolve("crypto-browserify"),
     stream: require.resolve("stream-browserify"),
-    assert: require.resolve("assert"),
     http: require.resolve("stream-http"),
     https: require.resolve("https-browserify"),
     os: require.resolve("os-browserify"),
