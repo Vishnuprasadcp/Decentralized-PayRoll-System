a9890e66ff7dde4da0cfeccf5d8c9f5f0669b3e8
Vishnuprasadcp <vishnuprasadcp07@gmail.com>
2025-03-05 14:35:35 +0530

    employee page is updated

--- dps/src/Pages/EmployeePage.js
+++ dps/src/Pages/EmployeePage.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import Employee from '../Components/Employee'
 
-function EmployeePage() {
-  return (
-    <div className="gradient-bg-welcome"><Employee/></div>
+function EmployeePage() {    <div className="gradient-bg-welcome"><Employee/></div>
   )
 }
 
